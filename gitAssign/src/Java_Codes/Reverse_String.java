abaec7e2f809d73b0479139ec72ba41236fa4c2c
Kulsherast Chauhan <kulsherast.chauhan@nagarro.com>
2023-09-25 22:50:16 +0530

    Java Codes Added Successfully

---
+++ gitAssign/src/Java_Codes/Reverse_String.java
@@ -0,0 +1,17 @@
+package Java_Codes;
+
+
+public class Reverse_String {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    String s = "prepinsta";
+	    String rev = "";
+
+	    for (int i = s.length()-1; i >=0; i--) {
+	      rev=rev+s.charAt(i);
+	    }
+	    System.out.println(rev);
+	  }
+
+}
